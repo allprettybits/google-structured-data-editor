713a91f7f8bc9b29af8e98ed136e4f8acef0cda2
Alexander Wende <alexanderwende@users.noreply.github.com>
2023-04-11 00:47:02 +0200

    fix: add favicon, screenshot, metadata and some minor style updates

--- rollup.config.js
+++ rollup.config.js
@@ -25,6 +25,7 @@ export default {
             patterns: [
                 'index.html',
                 'main.css',
+                'assets/**/*',
             ],
             rootDir: './src',
         }),
